c30fb48d809d9450f02772387cd4c6b35e39f78f
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 09:46:42 -0300

    chore: update shareCode to XJIRW0IDDUY

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=QGD2Z5981GU&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=XJIRW0IDDUY&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
